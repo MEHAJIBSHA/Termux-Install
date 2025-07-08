367d93d889e518e7899848431373ce40ae759dd1
MEHAJIBSHA <mehajibsha1998@gmail.com>
2025-07-08 13:49:44 +0530

    Update Create.py

--- Downloads/Create.py
+++ Downloads/Create.py
@@ -1,1 +1,16 @@
+### (Install (Termux)
 
+````bash
+pkg update -y && pkg upgrade -y && apt update -y && apt upgrade -y && pkg install git && pkg install python -y
+````
+
+
+### (Install The Dependencies)
+
+````bash
+git clone
+cd
+python
+python3
+node
+````
